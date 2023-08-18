9e6a74094ae43305af823656e4101bf9412900d6
Albertososa753 <betososa753@gmail.com>
2023-08-17 22:02:03 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
-import { Component } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
-import {   HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
 const APP_ROUTES: Routes = [
   {
     path: "home",
